1fd21a8df26485ecf6e41304d376b801b8d02f8c
Oliver Ayers <oliverdayers@gmail.com>
2020-09-14 15:43:37 -0700

    rebuilt database with new models - speakers with multiple truths

--- build_database.py
+++ build_database.py
@@ -1,36 +1,71 @@
 import os
+from datetime import datetime
 from config import db
-from models import Truth
+from models import Speaker, Truth
 
 # Data to initialize the database with
-TRUTHS = [
+# TRUTHS = [
+#     {
+#         'truthtext': 'Your silence will not protect you', 
+#         'speaker': 'Audre Lorde'
+#     },
+#     {
+#         'truthtext': 'Gender is a myth', 
+#         'speaker': 'Oliver Ayers'
+#     },
+#     {
+#         'truthtext': 'I like sunlight more than rain', 
+#         'speaker': 'Anonymous'
+#     },
+#     {
+#         'truthtext': 'I cried at the temple at Burning Man', 
+#         'speaker': 'Anonymous'
+#     },
+#     {
+#         'truthtext': 'Selfishness',
+#         'speaker': 'Anonymous'
+#     },
+#     {
+#         'truthtext': 'There is a war going on',
+#         'speaker': 'Bailey Davenport'
+#     }
+# ]
+
+# Data to initialize the database with
+SPEAKERS = [
     {
-        'truthtext': 'Your silence will not protect you', 
-        'speaker': 'Audre Lorde'
+        "name": "Audre Lorde",
+        "truths": [
+            ("Your silence will not protect you.", "1977-12-28 00:00:00"),
+        ],
     },
     {
-        'truthtext': 'Gender is a myth', 
-        'speaker': 'Oliver Ayers'
+        "name": "Oliver Ayers",
+        "truths": [
+            ("Gender is a myth", "1990-02-16 15:43:00"),
+        ],
     },
     {
-        'truthtext': 'I like sunlight more than rain', 
-        'speaker': 'Anonymous'
+        "name": "Bailey Davenport",
+        "truths": [
+            ("There is a war going on", "1990-01-02 00:00:00"),
+            ("Your silence will not protect you", "1990-01-02 01:01:01"),
+        ],
     },
     {
-        'truthtext': 'I cried at the temple at Burning Man', 
-        'speaker': 'Anonymous'
+        "name": "Anonymous",
+        "truths": [
+            ("I like sunlight more than rain", "2014-09-14 12:00:00"),
+            ("Selfishness", "2018-09-14 12:00:00"),
+            ("I cried at the temple at Burning Man", "2019-09-14 12:00:00"),
+        ],
     },
-    {
-        'truthtext': 'Selfishness',
-        'speaker': 'Anonymous'
-    },
-    {
-        'truthtext': 'There is a war going on',
-        'speaker': 'Anonymous'
-    }
 ]
 
 # Delete database file if it exists currently
+if os.path.exists('speakers.db'):
+    os.remove('speakers.db')
+
 if os.path.exists('truths.db'):
     os.remove('truths.db')
 
@@ -38,11 +73,21 @@ if os.path.exists('truths.db'):
 db.create_all()
 
 # Iterate over the TRUTHS structure and populate the database
-for truth in TRUTHS:
-    t = Truth(
-        truthtext=truth["truthtext"], 
-        speaker=truth["speaker"]
+for speaker in SPEAKERS:
+    s = Speaker(
+        name=speaker.get("name"),
     )
-    db.session.add(t)
+
+    # Add the truths for each person
+    for truth in speaker.get("truths"):
+        content, timestamp = truth
+        s.truths.append(
+            Truth(
+                content=content,
+                timestamp=datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S"),
+            )
+        )
+
+    db.session.add(s)
 
 db.session.commit()
--- models.py
+++ models.py
@@ -2,19 +2,50 @@ from datetime import datetime
 from config import db, ma
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, auto_field
 
-class Truth(db.Model):
-    __tablename__ = 'truth'
-    id = db.Column(db.Integer, primary_key=True)
-    speaker = db.Column(db.String(999))
-    truthtext = db.Column(db.String(999))
+class Speaker(db.Model):
+    __tablename__ = "speaker"
+    speaker_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
     timestamp = db.Column(
-        db.DateTime, 
-        default=datetime.utcnow, 
+        db.DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
+    # what does "delete-orphan" do? if it deletes truths that don't have speakers attached, I want to change that. truths that don't have speakers attached should be reassigned to Anonymous, not deleted. It looks like I'll have to change both delete-orphan and single-parent?
+    truths = db.relationship(
+        "Truth",
+        backref="speaker",
+        cascade="all, delete, delete-orphan",
+        single_parent=True,
+        order_by="desc(Truth.timestamp)"
+    )
+
+class Truth(db.Model):
+    __tablename__ = "truth"
+    truth_id = db.Column(db.Integer, primary_key=True)
+    speaker_id = db.Column(db.Integer, db.ForeignKey("speaker.speaker_id"))
+    # nullable = False means that it's okay to create new notes that have no content
+    content = db.Column(db.String, nullable=False)
+    timestamp = db.Column(
+        db.DateTime,
+        default=datetime.utcnow,
         onupdate=datetime.utcnow
     )
 
-class TruthSchema(ma.SQLAlchemyAutoSchema):
+# former Truth class
+# class Truth(db.Model):
+#     __tablename__ = 'truth'
+#     id = db.Column(db.Integer, primary_key=True)
+#     speaker = db.Column(db.String(999))
+#     truthtext = db.Column(db.String(999))
+#     timestamp = db.Column(
+#         db.DateTime, 
+#         default=datetime.utcnow, 
+#         onupdate=datetime.utcnow
+#     )
+
+class SpeakerSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        model = Truth
+        model = Speaker
         load_instance = True
         sqla_session = db.session
